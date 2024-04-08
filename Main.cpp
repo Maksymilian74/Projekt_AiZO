31aa701540e033219bdc2c8304672164b87ea289
Maksymilian74 <273007@student.pwr.edu.pl>
2024-04-09 01:16:36 +0200

    Initial commit

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include "Menu.h"
+
+class Main {
+public:
+    void run() {
+        Menu menu;
+        menu.runMainMenu();
+    }
+};
+
+int main() {
+    Main main;
+    main.run();
+    return 0;
+}
+
+
