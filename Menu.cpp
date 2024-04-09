bf17c6b9e767b95979d2a6c4dece694d2305a3cb
Maksymilian74 <273007@student.pwr.edu.pl>
2024-04-10 00:26:15 +0200

    Dodanie menu

--- Menu.cpp
+++ Menu.cpp
@@ -1,1 +1,37 @@
+#include "Menu.h"
+#include <iostream>
 
+using namespace std;
+
+void Menu::runMainMenu() {
+    int number;
+    do {
+        number = displayMainMenu();
+
+        switch (number) {
+            case 1:
+                cout << "Tryb pracy testowej\n";
+                break;
+            case 2:
+                cout << "Tryb pracy badawczej\n";
+                break;
+            case 3:
+                cout << "Zakonczenie dzialania programu\n";
+                break;
+            default:
+                cout << "Bledny numer, podaj prawidlowa wartosc\n";
+        }
+    } while (number != 6);
+}
+
+int Menu::displayMainMenu() {
+    int task;
+    cout << "---------------------------\n";
+    cout << "     --- Main Menu ---     \n";
+    cout << "1. Tryb pracy testowej     \n";
+    cout << "2. Tryb pracy badawczej    \n";
+    cout << "3. Wyjscie                 \n";
+    cout << "Wpisz numer zadania: ";
+    cin >> task;
+    return task;
+}
--- Menu.h
+++ Menu.h
@@ -1,1 +1,11 @@
+#include <iostream>
 
+using namespace std;
+
+class Menu {
+public:
+    void runMainMenu();
+
+private:
+    int displayMainMenu();
+};
